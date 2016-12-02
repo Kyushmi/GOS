ec9d2ddd91e5c3539f921bc93fc0c8b9168d77fa
Kyushmi <mmichuxdd@gmail.com>
2016-12-02 22:29:09 +0100

    Added Damage Indicator (idk how to write it xD)

--- K7Teemo.lua
+++ K7Teemo.lua
@@ -1,10 +1,10 @@
-local K7Version = "1.1"
+local K7Version = "1.2"
 
 function AutoUpdate(data)
     if tonumber(data) > tonumber(K7Version) then
-        PrintChat("<font color=\"#ffffff\">K7Teemo:</font> <font color=\"#adff2f\">New Version found</font> " .. data)
-        PrintChat("<font color=\"#ffffff\">K7Teemo:</font> <font color=\"#adff2f\">Downloading update, please wait...</font>")
-        DownloadFileAsync("https://raw.githubusercontent.com/Kyushmi/GoS/master/K7Teemo.lua", SCRIPT_PATH .. "K7Teemo.lua", function() PrintChat("<font color=\"#ffffff\">K7Teemo:</font> <font color=\"#adff2f\">Downloaded Update. Please 2x F6!</font>") return end)
+        PrintChat("<font color=\"#ffffff\"><b>K7Teemo:</b></font> <font color=\"#adff2f\">New Version found</font> " .. data)
+        PrintChat("<font color=\"#ffffff\"><b>K7Teemo:</b></font> <font color=\"#adff2f\">Downloading update, please wait...</font>")
+        DownloadFileAsync("https://raw.githubusercontent.com/Kyushmi/GoS/master/K7Teemo.lua", SCRIPT_PATH .. "K7Teemo.lua", function() PrintChat("<font color=\"#ffffff\"><b>K7Teemo:</b></font> <font color=\"#adff2f\">Downloaded Update. Please 2x F6!</font>") return end)
     else
 		PrintChat("<font color=\"#ffffff\">K7Teemo:</font> <font color=\"#adff2f\">No updates found!</font>")
     end
@@ -84,33 +84,107 @@ K7M.Gapcloser:Info("useQ", "Use Q on:")
 
 local TeemoR = {delay = 1.25, range = 900, radius = 250, speed = 1200}
 
+
+local function TGetHPBarPos(enemy)
+  local barPos = GetHPBarPos(enemy) 
+  local BarPosOffsetX = -50
+  local BarPosOffsetY = 46
+  local CorrectionY = 39
+  local StartHpPos = 31 
+  local StartPos = Vector(barPos.x , barPos.y, 0)
+  local EndPos = Vector(barPos.x + 108 , barPos.y , 0)    
+  return Vector(StartPos.x, StartPos.y, 0), Vector(EndPos.x, EndPos.y, 0)
+end
+
+local function DrawLineHPBar(text, unit, team)
+  if unit.dead or not unit.visible then return end
+  local p = WorldToScreen(0, Vector(unit.x, unit.y, unit.z))
+  local thedmg = 0
+  local line = 2
+  local linePosA  = { x = 0, y = 0 }
+  local linePosB  = { x = 0, y = 0 }
+  local TextPos   = { x = 0, y = 0 }
+
+  if getdmg("Q", unit) >= unit.health then
+    thedmg = unit.health - 1
+    text = "KILLABLE FROM Q!"
+  else
+    thedmg = getdmg("Q", unit)
+    text = "Damage from Q"
+  end
+
+  thedmg = math.round(thedmg)
+
+  local StartPos, EndPos = TGetHPBarPos(unit)
+  local Real_X = StartPos.x + 24
+  local Offs_X = (Real_X + ((unit.health - thedmg) / unit.maxHealth) * (EndPos.x - StartPos.x - 2))
+  if Offs_X < Real_X then Offs_X = Real_X end 
+  local mytrans = 350 - math.round(255*((unit.health-thedmg)/unit.maxHealth))
+  if mytrans >= 255 then mytrans=254 end
+  local my_bluepart = math.round(400*((unit.health-thedmg)/unit.maxHealth))
+  if my_bluepart >= 255 then my_bluepart=254 end
+
+  if team then
+    linePosA.x = Offs_X - 24
+    linePosA.y = (StartPos.y-(30+(line*15)))    
+    linePosB.x = Offs_X - 24 
+    linePosB.y = (StartPos.y+10)
+    TextPos.x = Offs_X - 20
+    TextPos.y = (StartPos.y-(30+(line*15)))
+  else
+    linePosA.x = Offs_X-125
+    linePosA.y = (StartPos.y-(30+(line*15)))    
+    linePosB.x = Offs_X-125
+    linePosB.y = (StartPos.y-15)
+
+    TextPos.x = Offs_X-122
+    TextPos.y = (StartPos.y-(30+(line*15)))
+  end
+
+  DrawLine(linePosA.x, linePosA.y, linePosB.x, linePosB.y , 2, ARGB(mytrans, 255, my_bluepart, 0))
+  DrawText(tostring(thedmg).." "..tostring(text), 15, TextPos.x, TextPos.y , ARGB(mytrans, 255, my_bluepart, 0))
+end
+
+
 OnDraw(function()
 	
-	if K7M.Draws.drawReady:Value() then
+	if not localplayer.dead then
 
-		if CanUseSpell(localplayer,_Q) == READY and K7M.Draws.drawQ:Value() then
-			DrawCircle(localplayer,GetCastRange(localplayer,_Q),1,25,GoS.Blue)
+		if K7M.Draws.drawReady:Value() then
+
+			if CanUseSpell(localplayer,_Q) == READY and K7M.Draws.drawQ:Value() then
+				DrawCircle(localplayer,GetCastRange(localplayer,_Q),1,25,GoS.Blue)
+			end
+
+			if CanUseSpell(localplayer,_R) == READY and K7M.Draws.drawR:Value() then
+				DrawCircle(localplayer,GetCastRange(localplayer,_R),1,25,GoS.Yellow)
+			end
+
+		else
+
+			if K7M.Draws.drawQ:Value() then
+				DrawCircle(localplayer,GetCastRange(localplayer,_Q),1,25,GoS.Blue)
+			end
+
+			if K7M.Draws.drawR:Value() then
+				DrawCircle(localplayer,GetCastRange(localplayer,_R),1,25,GoS.Yellow)
+			end
 		end
 
-		if CanUseSpell(localplayer,_R) == READY and K7M.Draws.drawR:Value() then
-			DrawCircle(localplayer,GetCastRange(localplayer,_R),1,25,GoS.Yellow)
-		end
+		for i, Enemy in pairs(GetEnemyHeroes()) do
+        	if not Enemy.dead and Enemy.visible then
+            	if localplayer:CanUseSpell(_Q) == READY and not Enemy.dead then
+            		DrawLineHPBar("", Enemy, Enemy.team)
+           		end
+        	end 
+    	end
 
-	else
-
-		if K7M.Draws.drawQ:Value() then
-			DrawCircle(localplayer,GetCastRange(localplayer,_Q),1,25,GoS.Blue)
-		end
-
-		if K7M.Draws.drawR:Value() then
-			DrawCircle(localplayer,GetCastRange(localplayer,_R),1,25,GoS.Yellow)
-		end
 	end
 end)
 
 function CastR(unit)
 	local predictR = GetCircularAOEPrediction(unit, TeemoR)
-	if predictR.hitChance >= (K7M.Pred.HitchanceR:Value()*0.01) then
+	if predictR.hitChance >= (K7M.Pred.HitchanceR:Value() * 0.01) then
 		CastSkillShot(_R, predictR.castPos)
 	end
 end
@@ -224,4 +298,4 @@ end)
 
 
 AddGapcloseEvent(_Q, 680, true, K7M.Gapcloser)
-PrintChat("<font color=\"#ffffff\">K7Teemo Toxic Player:</font> <font color=\"#adff2f\">Injected successfully!</font>")
+PrintChat("<font color=\"#ffffff\"><b>K7Teemo Toxic Player:</b></font> <font color=\"#adff2f\">Injected successfully!</font>")
